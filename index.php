f1c7d203e1b660b20f83fc9c90277e88ec6d9325
stshila94 <149912854+stshila94@users.noreply.github.com>
2023-11-05 15:29:07 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,9 @@ echo 'Hello World: I am here for developing wordpress template and website';
 echo 'We are checking git push update';
 echo '<input type="text" name="email">';
 ?>
+
+<div class="header">
+
+  <h1>This is index header from GitHub edit</h1> 
+  
+</div>
